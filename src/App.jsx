e35e3759f8e32984523b30261cbf6c04f09f1da1
bigphu <bigphu2005@gmail.com>
2025-11-19 20:24:57 +0700

    Use React Router DOM to avoid full reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,31 @@
-import React from 'react'
-import Navbar from './components/nav-bar/Navbar.jsx'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import MyLinks from './pages/MyLinks.jsx'
-import Discovery from './pages/Discovery.jsx'
-import Profile from './pages/Profile.jsx'
+import Navbar from './components/nav-bar/Navbar.jsx';
+
+import Home from './pages/Home.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import MyLinks from './pages/MyLinks.jsx';
+import Discovery from './pages/Discovery.jsx';
+import Profile from './pages/Profile.jsx';
 
 const App = () => {
-  console.log(window.location.pathname)
-
-  let PageToRender;
-  switch (window.location.pathname) {
-    case '/':
-      PageToRender = <Home />;
-      break
-    case '/dashboard':
-      PageToRender = <Dashboard />;      
-      break
-    case '/mylinks':
-      PageToRender = <MyLinks />;      
-      break
-    case '/discovery':
-      PageToRender = <Discovery />;      
-      break
-    case '/profile':
-      PageToRender = <Profile />;
-      break
-    default:
-      PageToRender = <div>404 Not Found</div>
-  }
-  
   return (
     <div type='module'>
       <Navbar />
-      {PageToRender}
+
+      <div className='spacer-navbar'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/mylinks' element={<MyLinks />} />
+          <Route path='/discovery' element={<Discovery />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='*' element={<div>404 Not Found</div>} />
+        </Routes>   
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/nav-bar/Navbar.jsx
+++ src/components/nav-bar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 import './Navbar.css'
 
 import logo from '../../assets/logo.png'
@@ -7,51 +9,41 @@ import profileIcon from '../../assets/profile-icon.svg'
 
 const Navbar = () => {
   return (
-    <nav className="navbar-wrapper"> 
+    <nav className='navbar-wrapper'> 
       {/* LEFT SIDE */}
-      <div className="navbar-left">
-        <a href="/">
+      <div className='navbar-left'>
+        <NavLink to='/' className='navbar-icon-home'>
           <img src={logo} 
-          alt="Logo" 
-          className="navbar-logo" />
-        </a>
+          alt='Logo' 
+          className='navbar-logo' />
+        </NavLink>
 
-        <div className="navbar-links">
-          <CustomLink href="/">Home</CustomLink>
-          <CustomLink href="/dashboard">Dashboard</CustomLink>
-          <CustomLink href="/mylinks">My Links</CustomLink>
-          <CustomLink href="/discovery">Discovery</CustomLink>
+        <div className='navbar-links'>
+          <NavLink to='/' className='navbar-link-home'>Home</NavLink>
+          <NavLink to='/dashboard' className='navbar-link-dashboard'>Dashboard</NavLink>
+          <NavLink to='/mylinks' className='navbar-link-mylinks'>My Links</NavLink>
+          <NavLink to='/discovery' className='navbar-link-discovery'>Discovery</NavLink>
         </div>
       </div>
 
       {/* RIGHT SIDE */}
-      <div className="navbar-right">
+      <div className='navbar-right'>
         <img
           src={notificationIcon}
-          alt="Notifications"
-          className="navbar-notification-icon"
+          alt='Notifications'
+          className='navbar-notification-icon'
         />
         
-        <a href="/profile">
+        <NavLink to='/profile'>
           <img
             src={profileIcon}
-            alt="User Profile"
-            className="navbar-profile-icon"
+            alt='User Profile'
+            className='navbar-profile-icon'
           />
-        </a>
+        </NavLink>
       </div>
     </nav>
   )
-}
-
-function CustomLink({href, children, ...props}) {
-  const PATH = window.location.pathname;
-
-  if (PATH === href) {
-    return <a href={href} className="active" {...props}>{children}</a>
-  } else {
-    return <a href={href} {...props}>{children}</a>
-  }
 }
 
 export default Navbar
