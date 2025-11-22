53ec6a81ed160f2ae7c1b7a9b822c0e9f3a269f4
bigphu <bigphu2005@gmail.com>
2025-11-22 10:14:57 +0700

    added Home button to page-404

--- src/pages/page-404/Page404.jsx
+++ src/pages/page-404/Page404.jsx
@@ -4,14 +4,19 @@ import img404 from '../../assets/page-404.jpg'
 import './Page404.css'
 
 import PageLayout from '../../components/page-layout/PageLayout'
+import Button from '../../components/button/Button'
+import { useNavigate } from 'react-router-dom'
 
 const Page404 = () => {
+  const navigate = useNavigate();
+
   return (
     <PageLayout>
       <div className='page404 row'>
-        <img src={img404}></img>
+        <img src={img404} alt='Error'></img>
 
         <div className='text-big text-dark text-bold'>Error 404: Page not found (T^T) </div>
+        <Button type='ghost' text='Back to home' onClick={() => navigate('/')} />
       </div>
     </PageLayout>
   )
